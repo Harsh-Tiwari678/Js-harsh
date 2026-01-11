ee24fd9f34a47ebc41a0178a47420a4c5c1049ea
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-01-11 23:41:32 +0530

    learning DOM Manipulation

---
+++ DOM/script.js
@@ -0,0 +1,32 @@
+/*let abcd= document.getElementById("abcd");
+console.dir(abcd); // selection by id 
+
+let abcd= document.getElementsByClassName("abcd");
+console.dir(abcd); // selection by class name
+
+let abcd = document.querySelectorAll("h1");
+console.dir(abcd); // by query selector all 
+  
+let abcd= document.querySelector("h1");
+ console.dir(abcd); // by query selector 
+
+ let h1= document.querySelector("h1");
+ h1.innerHTML="<i>hiii harsh kaise ho</>"
+ console.dir(h1) // inner html changes the html code  
+
+ let a = document.querySelector("a");
+ a.href="https://www.google.com"; // one way to do the attribute maipulation 
+
+
+let img = document.querySelector("img");
+img.setAttribute("src","https://imgs.search.brave.com/EnJsVhXmIvL_CJ2ieoRe4iAeAkvRJ_yjm9BNbRhdwbM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9zb21l/LW15LW1vcm5pbmct/cGljdC0yNjI2NjA2/ODAuanBn");
+// attribute manipulation by set attribute
+
+let a = document.querySelector("a");
+console.log(a.getAttribute("href")); // getting the value of href by using get attribute 
+*/
+let a = document.querySelector("a");
+a.removeAttribute("href"); // remove the href attribute from the a tag 
+
+
+
