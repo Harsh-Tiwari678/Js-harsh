c7af1d6f404fcbacf16dcf5ecdc38f0ee4c49eb9
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-04-18 00:00:59 +0530

    this project in progress

--- advanceJS/thisProject.js
+++ advanceJS/thisProject.js
@@ -1,14 +1,27 @@
 let form  = document.querySelector("form");
+let username  = document.querySelector("#name");
+let bio  = document.querySelector("#bio");
+let role  = document.querySelector("#role");
+let img  = document.querySelector("#photo");
 
 const userManager = {
    user : [] ,// isme sare users aayenge 
-   init : function(){
-     form.addEventListener("submit",function(val){
-        val.preventDefault();
-        console.log(this); // here this is the form 
-     })
-   }, // kaam hai shuruwat mein chale 
-   adduser: function(){},
+   init : function(){   // kaam hai shuruwat mein chale 
+     form.addEventListener("submit",this.submitform.bind(this)) // yha this ye pura object hai // jab bhi submit hoga this.submit form chal jayega // yha this bind mein isliye likha hai bcs this yha object haiham suermanagaer bhi likh sakte hai 
+   },
+    submitform: function(val){
+   val.preventDefault();
+      this.adduser();
+    },
+   adduser: function(){
+       this.user.push({ // this.user mein push kardo ek naya object 
+        username : username.value,
+        bio : bio.value,
+        role : role.value,
+        img : img.value,                 
+    })
+    form.reset();
+   },
    removeUser : function(){}
 }
  userManager.init(); // bydefault chla rha hai 
