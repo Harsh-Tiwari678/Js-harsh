1043e19aaacbd57a829e9a3b8cf548ee3e1e4eb0
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-04-19 10:57:02 +0530

    constructor function and prototype done

--- advanceJS/OOPS.js
+++ advanceJS/OOPS.js
@@ -7,19 +7,22 @@
 //   let Pencil1 = new Pencil("Natrajan", "Doms", 100, "blue"); // new ke lagne se this is the blank object 
 //   let Pencil2 = new Pencil("DOMS", "DOMS",12,"black")
 
+// prototype-------------------------------
 
-function Pencil(name , company, price , color){
+
+function Pencil(name , price , color){
   this.name = name;  // this yha pe abhi ek blank object maan lo 
-  this.company = company;
   this.price = price;
   this.color  = color;
-  this.write = function(text){
+  
+}
+   Pencil.prototype.write = this.write = function(text){
   let h1 =   document.createElement("h1");
   h1.textContent = text;
-  h1.style.color = color;
-  document.body.appendChild(h1);
+  h1.style.color = this.color;
+  document.body.appendChild(h1);   // yha pe ab ye write function shared memory ein chala jaeyga  dono instances ki .. thus we save memory 
 
   }
-}
-  let Pencil1 = new Pencil("Natrajan", "Doms", 100, "blue"); // new ke lagne se this is the blank object 
-  let Pencil2 = new Pencil("DOMS", "DOMS",12,"black")
+  
+  let Pencil1 = new Pencil("Natrajan", 100, "blue"); // new ke lagne se this is the blank object 
+  let Pencil2 = new Pencil("DOMS",12,"black")
