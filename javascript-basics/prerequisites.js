cb9ddcaa11deed45c54c1e489ada857d7d58d76f
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-07-15 13:19:00 +0530

    revised objects

---
+++ javascript-basics/prerequisites.js
@@ -0,0 +1,52 @@
+// var a = 10 ;
+// hello = ()=>{
+//     var a = 20;
+//     console.log(a);
+// }
+// hello();
+// console.log(a);
+
+
+// // therefore var is functional scoped 
+
+
+// const obj  = {
+//     name : "John",
+// }
+// obj.name = "Harsh";
+// console.log(obj.name);   // Harsh 
+
+// const obj  = {}  // error 
+
+
+// const can only change the properties of the object but prevnets the variable referance 
+
+
+
+//objects 
+
+// user data objeects -> this tell us what proeprtie does the uer have and what data is stored in the object
+
+//  let user  = {
+//     name : "Harsh",
+//     email :   "harshtiwari1806@gmail.com",
+//     id : 101,
+//     adress : "India",
+// }
+// console.log(user.name);
+// console.log(user.email);
+
+
+// const user = { 
+//     name : "Harsh",
+// }
+// const anotheruser = user;  // here we just create another referace to the same object in the memory 
+// anotheruser.name = "Piyush";
+// console.log(user.name);
+
+// const api_req = {
+//     meathod  : "POST",
+//     endpoint :  "/api/users",
+//     autheticated : true,
+
+// }
