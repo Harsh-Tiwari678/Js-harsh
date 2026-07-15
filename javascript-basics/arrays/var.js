03b1dea0d62504767d991bc78ffd994bc483d56a
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-07-15 11:32:21 +0530

    refactor: update object manipulation examples and clarify const behavior

--- javascript-basics/arrays/var.js
+++ javascript-basics/arrays/var.js
@@ -8,3 +8,15 @@ console.log(a);
 
 
 // therefore var is functional scoped 
+
+
+const obj  = {
+    name : "John",
+}
+obj.name = "Harsh";
+console.log(obj.name);   // Harsh 
+
+const obj  = {}  // error 
+
+
+// const can only change the properties of the object but prevnets the variable referance 
